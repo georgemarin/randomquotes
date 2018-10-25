3e7786ff3bacaed73dcab101a6cde9353ddef58a
georgemarin <marin_george51@hotmail.com>
2018-10-25 18:09:12 +0300

    Deploy

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ const app = express();
   app.use(express.json());
   app.use(express.urlencoded({extended: false}));
   app.use(cookieParser());
-  app.use(express.static(path.join(__dirname, 'public')));
   app.use(bodyParser.urlencoded({extended: true}))
 
   app.use('/', indexRouter);
@@ -42,15 +41,13 @@ const app = express();
 
   await db.connect();
 
-  if (process.env.NODE_ENV === 'production') {
-    // Serve any static files
     app.use(express.static(path.join(__dirname, 'client/build')));
 
     // Handle React routing, return all requests to React app
     app.get('*', function (req, res) {
       res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
-  }
+
 }());
 
 module.exports = app;
