301c96d98daab730d707878a0865f9e5c1077f82
georgemarin <marin_george51@hotmail.com>
2018-10-26 10:23:17 +0300

    Remove code

--- app.js
+++ app.js
@@ -9,42 +9,26 @@ const { db } = require('./lib/db');
 
 const app = express();
 
-(async function startServer() {
+async function startServer() {
 
   app.use(logger('dev'));
   app.use(express.json());
   app.use(express.urlencoded({extended: false}));
   app.use(cookieParser());
   app.use(bodyParser.urlencoded({extended: true}))
-  app.use('/api', indexRouter);
-
-// catch 404 and forward to error handler
-  app.use((req, res, next) => {
-    next(createError(404));
-  });
-
-// error handler
-  app.use((err, req, res, next) => {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-  });
 
   await db.connect();
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use('/api', indexRouter);
 
-    app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client/build/index.html'));
+  });
 
-    // Handle React routing, return all requests to React app
-    app.get('*', function (req, res) {
-      res.sendFile(path.join(__dirname, 'client/build/index.html'));
-    });
+  const port = process.env.PORT || 5000;
+  app.set('port', port);
+  app.listen(port, () => console.log(`App listening on port ${port}!`));
+}
 
-}());
-
-
-
-module.exports = app;
+startServer().catch(console.log);
