5a25fb0a8b018af63d54f54c74abd716509c83b0
lauramaria <lauramariapacurar@yahoo.com>
2018-10-30 19:58:23 +0200

    added SyncLoader

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 import SimpleCard from "./SimpleCard";
 import Grid from "@material-ui/core/Grid/Grid";
+import {SyncLoader} from 'react-spinners';
 
 class Main extends Component {
   static propTypes = {
@@ -15,16 +16,39 @@ class Main extends Component {
     this.props.quotesActions.fetchQuotes();
     this.state = {
       quote: {},
+      loading: true,
     };
   };
 
-  render () {
+  render() {
+
     if (!this.props.quotes.hasLoaded) {
-      return (<div>Loading data...</div>);
+      return (
+        <div
+          style={{display: 'table', position: 'absolute', height: '100%', width: '100%', backgroundColor: '#002b38'}}>
+          <div style={{display: 'table-cell', verticalAlign: 'middle'}}>
+            <div className=".col-md-12" style={{marginLeft: 'auto', marginRight: 'auto'}}>
+              <Grid
+                container
+                direction="column"
+                justify="center"
+                alignItems="center"
+              >
+                <SyncLoader
+                  sizeUnit={"px"}
+                  size={35}
+                  color={'white'}
+                  loading={this.state.loading}
+                />
+              </Grid>
+            </div>
+          </div>
+        </div>
+      );
     }
 
     return (
-      <div style={{ display: 'table', position: 'absolute', height: '100%', width: '100%', backgroundColor: '#002b38' }}>
+      <div style={{display: 'table', position: 'absolute', height: '100%', width: '100%', backgroundColor: '#002b38'}}>
         <div style={{display: 'table-cell', verticalAlign: 'middle'}}>
           <div className=".col-md-12" style={{marginLeft: 'auto', marginRight: 'auto'}}>
             <Grid
@@ -34,10 +58,10 @@ class Main extends Component {
               alignItems="center"
             >
               <SimpleCard
-                  quotes={this.props.quotes.quotes}
-                  updateQuote={this.props.quotesActions.updateQuote}
+                quotes={this.props.quotes.quotes}
+                updateQuote={this.props.quotesActions.updateQuote}
               />
-                {console.log(this.props.quotes.quotes)}
+              {console.log(this.props.quotes.quotes)}
             </Grid>
           </div>
         </div>
@@ -46,8 +70,8 @@ class Main extends Component {
   }
 }
 
-const mapState = ({ quotes }) => ({ quotes });
-const mapDispatch = ({ quotes }) => ({
+const mapState = ({quotes}) => ({quotes});
+const mapDispatch = ({quotes}) => ({
   quotesActions: quotes,
 });
 
